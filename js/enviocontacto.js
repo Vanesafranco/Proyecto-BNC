762338dcd91879ffec286b73df511f9607a4ac82
Vanesafranco <vanessafranco96@outlook.com>
2022-12-03 15:25:51 -0500

    final

---
+++ js/enviocontacto.js
@@ -0,0 +1,17 @@
+//declarar unas variables, para capturar lo que se encuentra en los id
+const formcontact = document.querySelector('#formcontact');
+const recepcion = document.querySelector('#recepcion');
+
+//crear un evento de escucha para el submit o envio de correo
+//crear una funcion co9n el nombre de enviarInfo, para ejecutar el envio de correo
+formcontact.addEventListener('submit',enviarInfo);
+
+function enviarInfo(event){
+    //crear un envento de presionar el boton envio
+    event.preventDefault();
+    //vamos a capturar lo que hay en los elementos del formulario
+    const envioForm = new FormData (this);
+    //recepcion de los datos
+    recepcion.setAttribute('href',`mailto:${envioform.get('email')}?subject=nombre: ${envioForm.get('nombre')} apellido: ${envioForm.get('apellido')} email: ${envioForm.get ('email')}&body=${envioForm.get ('mensaje')}`);
+    recepcion.click();
+}
